f209cc6cd1536a66477a0facfd661ecdede28c8e
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-09-18 15:10:42 -0400

    Fixed viewEmployee to show the correct info

--- server.js
+++ server.js
@@ -113,7 +113,13 @@ const mainMenu = () => {
 
 // Define a function to view employees
 const viewEmployees = () => {
-  sql = `SELECT * FROM employee`;
+  sql = 
+  `SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, role.salary, department.name AS department, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN employee AS manager ON employee.manager_id = manager.id  
+  LEFT JOIN role ON employee.role_id = role.id
+  LEFT JOIN department ON role.department_id = department.id;
+  `;
   db.query(sql, (err, rows) => {
     if (err) {
       console.log(err);
